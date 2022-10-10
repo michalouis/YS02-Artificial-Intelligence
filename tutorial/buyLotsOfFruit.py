098f45e959d40ee68251e1d47df413416ee30a8a
michalouis <michael.aretakis2002@gmail.com>
2022-10-10 20:19:11 +0300

    Comment removed from q2

--- tutorial/buyLotsOfFruit.py
+++ tutorial/buyLotsOfFruit.py
@@ -35,7 +35,6 @@ def buyLotsOfFruit(orderList):
     Returns cost of order
     """
     totalCost = 0.0
-    "*** YOUR CODE HERE ***"
 
     for fruit in orderList:
         if fruit[0] in fruitPrices:
