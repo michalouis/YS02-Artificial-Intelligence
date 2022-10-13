1577814ac9f1aa66ff7811f6e5848ad35e17118c
michalouis <michael.aretakis2002@gmail.com>
2022-10-13 15:51:23 +0300

    added examples to main

--- Project0/PriorityQueue/pqueue.py
+++ Project0/PriorityQueue/pqueue.py
@@ -107,9 +107,40 @@ def PQSort(list):
 if __name__ == '__main__':
     q = PriorityQueue()
 
-    
+    # PriorityQueue class example
+    print("### PriorityQueue class example ###")
+    print("Add (""task1"", 1) to PriorityQueue")
+    q.push("task1", 1)
+    print("q.heap = %s\n" % q.heap)
 
+    print("Remove (""task1"", 1) from PriorityQueue")
+    q.pop()
+    print("q.heap = %s\n" % q.heap)
+
+    print("Add items to PriorityQueue")
+    q.push("task1", 3)
+    q.push("task1", 4)
+    q.push("task1", 5)
+    q.push("task2", 2)
+    q.push("task2", 4)
+    q.push("task2", 6)
+    q.push("task2", 8)
+    q.push("task3", 5)
+    q.push("task3", 10)
+    q.push("task3", 15)
+    print("q.heap = %s\n" % q.heap)
+
+    print("Update task2 priority to 3")
+    q.update("task2", 3)    
+    print("q.heap = %s\n" % q.heap)
+
+    print("Update task3 priority to 4")
+    q.update("task3", 4)    
+    print("q.heap = %s\n" % q.heap)
+
+    # PQSort function
+    print("### PQSort function example ###")
     list = [1, 3, 5, 8, 2, 4, 4, 6, 6, 6, 9, 7]
-    print("Before PQsort: %s" % list)
+    print("List before PQsort: %s" % list)
     PQSort(list)
-    print("After PQsort: %s" % list)
+    print("List after PQsort: %s" % list)
